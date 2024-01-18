32a3fa21f7693a97f19adbf6c6d81bb9b3142b8d
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-01-18 19:03:48 +0700

    feat: improve homepage view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,20 +15,6 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
@@ -36,14 +22,14 @@
                     </svg>
                 </div>
 
-                <div class="flex justify-center mt-16 px-0 sm:items-center">
+                <div class="flex justify-center mt-16 px-0">
                     <div class="text-center text-4xl text-gray-500 dark:text-gray-400 sm:ml-0">
-                        Laravel AutoDeploy
+                        Laravel AutoDeploy v1.0.0
                     </div>
                 </div>
 
-                <div class="flex mt-16 px-0 sm:items-center sm:justify-between">
-                    <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:ml-0">
+                <div class="mt-5 px-0">
+                    <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
